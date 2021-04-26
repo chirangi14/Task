490a486b8247171223efc4d42beae468282ac2c7
Chirangi Sukhadiya <chirangisukhadiya58@gmail.com>
2021-04-27 01:03:09 +0530

    my project

---
+++ update.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <title>Student Details</title>
+        <meta name="description" content="A sidebar menu as seen on the Google Nexus 7 website" />
+        <meta name="keywords" content="google nexus 7 menu, css transitions, sidebar, side menu, slide out menu" />
+        <meta name="author" content="Codrops" />
+        <link rel="shortcut icon" href="../favicon.ico">
+        <link rel="stylesheet" type="text/css" href="css/normalize.css" />
+        <link rel="stylesheet" type="text/css" href="css/demo.css" />
+        <link rel="stylesheet" type="text/css" href="css/component.css" />
+        <script src="js/modernizr.custom.js"></script>
+<style>
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  background-color: grey;
+  margin-left: 370px;
+  margin-right: 320px;
+  margin-bottom: 70px;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+/* Add padding to containers */
+.container {
+  padding: 16px;
+  background-color: white;
+}
+
+/* Full-width input fields */
+input[type=text], input[type=email], input[type=number], input[type=file] {
+  width: 100%;
+  padding: 15px;
+  margin: 5px 0 22px 0;
+  display: inline-block;
+  border: none;
+  background: #f1f1f1;
+}
+
+input[type=text]:focus , input[type=email]:focus, input[type=number]:focus, input[type=file]:focus {
+  background-color: #ddd;
+  outline: none;
+}
+
+.spec , .gender
+{
+    width: 100%;
+  padding: 15px;
+  margin: 5px 0 22px 0;
+  display: inline-block;
+  border: none;
+  background: #f1f1f1;
+  cursor: pointer;
+}
+.spec:focus , .gender:focus
+{
+    background-color: #ddd;
+    outline: none;
+}
+/* Overwrite default styles of hr */
+hr {
+  border: 1px solid #f1f1f1;
+  margin-bottom: 25px;
+}
+
+/* Set a style for the submit button */
+.registerbtn {
+  background-color: #4CAF50;
+  color: white;
+  padding: 16px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+  opacity: 0.9;
+}
+
+.registerbtn:hover {
+  opacity: 1;
+}
+
+/* Add a blue text color to links */
+a {
+  color: dodgerblue;
+}
+
+/* Set a grey background color and center the text of the "sign in" section */
+/*.signin {
+  background-color: #f1f1f1;
+  text-align: center;
+}*/
+</style>
+</head>
+<body>
+
+
+<br/><br/><br/>
+<form action="stud_details.php" method="POST" enctype="multipart/form-data">
+  <div class="container">
+    <h1>Student Details</h1>
+    <p>Please fill this form </p>
+    <hr>
+
+    <label ><b>Name</b></label>
+    <input type="text" placeholder="Enter  Name" name="stud_name" id="stud_name" required pattern="[a-zA-Z .]+" title="It should be characters only">
+
+    <label ><b>Email</b></label>
+    <input type="email" placeholder="Enter Email" name="email" id="email" required>
+    
+    <label ><b>Mobile Number</b></label>
+    <input type="number" placeholder="Enter Mobile Number" name="mob_no" id="mob_no" required  title="It should be numbers only">
+
+    
+    <label ><b>State</b></label>
+    
+    <select name="state" class="spec" required>
+    <option value="" disabled="disabled" selected="true">Select One</option>
+    <option value="Gujarat">Gujarat</option>
+    <option value="Maharastra">Maharastra</option>
+
+    </select>
+    
+    
+    
+    <label ><b>City</b></label>
+    
+    <select name="city" class="spec" required>
+    <option value="" disabled="disabled" selected="true">Select One</option>
+    <option value="Surat">Surat</option>
+    <option value="Pune">Pune</option>
+    </select>
+
+    <label ><b>Address</b></label>
+    <input type="text" placeholder="Enter address" name="address" id="address" required>
+    <hr>
+    
+    <a href="stud_details.php">
+      <button type="update" class="registerbtn" name="update" >Update</button>
+
+    </a>
+    
+   
+
+
+
+  </div>
+  
+</form>
+
+</body>
+</html>
+
+
+
+
